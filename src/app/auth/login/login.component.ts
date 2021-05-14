dc183bf88a0390bdd463f24b755212663b545f80
Sherry <asdfg5665@gmail.com>
2021-05-14 10:13:07 -0700

    [0514] class material - binding model (login) & read URL id (movie-card-list)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from 'src/app/shared/models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userLogin: Login = {
+    email:'',password:''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.userLogin);
   }
 
+  login()
+  {
+    console.log(this.userLogin);
+    console.log('button click');
+  }
+
+  // simply observing two way binding, just for testing, remove it later
+  get twoWayBindingInfo() { return JSON.stringify(this.userLogin) }
 }
--- src/app/movies/movie-card-list/movie-card-list.component.ts
+++ src/app/movies/movie-card-list/movie-card-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GenreService } from 'src/app/core/services/genre.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card-list',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieCardListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private genreservice: GenreService, private route: ActivatedRoute, private router: Router) { }
+
+  genreId: number | undefined;
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      params => {
+        this.genreId = params.get('id') ? Number(params.get('id')) : 0;
+        console.log(this.genreId);
+        // call your movie service
+      }
+    );
   }
 
 }
